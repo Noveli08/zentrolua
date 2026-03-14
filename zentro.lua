dfbb02f1da3c0c5f58b883ef433a2907db2d9110
best2322 <kusaikusai4455@gmail.com>
2026-03-14 22:44:29 +0100

    Update zentro.lua

--- zentro.lua
+++ zentro.lua
@@ -232,7 +232,7 @@ end)
 ------------------------------------------------
 -- KEY SYSTEM LOGIC
 ------------------------------------------------
-local correctKey = "sorrykey"
+local correctKey = "goofyshit"
 
 enter.MouseButton1Click:Connect(function()
 	local entered = string.lower(keyBox.Text:gsub("%s+",""))
